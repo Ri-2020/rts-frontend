e56741b4e60210cc0b3ea35ea2b9d82a00a35939
Ri-2020 <rohitgupta111abcd@gmail.com>
2023-01-29 19:07:38 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App_css/App.css';
 import Home  from './Home/home';
+import Dashboard from './Dashboard/Dashboard';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-     <Home/>
-    </div>
+    // <Router>
+        <div className="App">
+
+          <Router>
+            <Routes>
+                <Route index element={<Home />} />
+                <Route path="dashboard" element={<Dashboard />} />
+            </Routes>
+          </Router>
+          </div>
   );
 }
 
--- src/Home/home.jsx
+++ src/Home/home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home() {
   return (
-    <>
+    <div className="homeBox">
         <div  className=' darkBorder headerBorder'>
                 welcome to raah towards success
         </div>
@@ -39,7 +39,7 @@ function Home() {
         <div className='meme'>
             <img src='https://preview.redd.it/cqzm8q6sgal61.png?auto=webp&s=a4d968d60e307a790fcd9340285099dd48073ed9' alt='it is supposed to be meme? where is it? hmm.'/>
         </div>
-    </>
+    </div>
   )
 }
 
