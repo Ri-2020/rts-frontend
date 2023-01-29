d82de5b60b7edc541d829b2f72c4ae8f26976948
Ri-2020 <rohitgupta111abcd@gmail.com>
2023-01-29 19:31:46 +0530

    dashboard added

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import './dashboard.css'
 
 export const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
+    <div className="dashboardBox">
+        <div className='dashboard_header'>
+            <div  className=' darkBorder headerBorder'>
+                    Hi, Rohit Gupta (20-0736-013-0047)
+            </div>
+            <div>
+                    <div className='dashboard_header_button'>
+                        <div className='inside'>
+                            edit
+                        </div>
+                    </div>
+                    <div className='submitButton'>
+                        <div className='inside'>
+                            home
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </div>
+  );
 }
 
 export default Dashboard;
